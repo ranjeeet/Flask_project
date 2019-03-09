089e207ec1f96180bcef6c8d5390253fb93f2def
ranjeeet <thechauhan97@gmail.com>
2019-03-09 14:37:48 +0530

    Version 1.6

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from datetime import datetime
 from werkzeug.utils import secure_filename
 from flask import request
 import pymysql
+import math
 import json
 from flask_mail import Mail
 
@@ -59,8 +60,29 @@ class Posts(db.Model):
 
 @app.route("/")
 def home():
-    posts = Posts.query.filter_by().all()[0:params['no_of_posts']]
-    return render_template('index.html', params=params, posts=posts)
+    posts = Posts.query.filter_by().all()
+    last = math.ceil(len(posts)/int(params['no_of_posts']))
+    #[0:params['no_of_posts']]
+    #pagination
+
+    page = (request.args.get('page'))
+    if not (str(page).isnumeric()):
+        page = 1
+    page = int(page)
+    posts = posts[(page-1)*int(params['no_of_posts']): (page-1)*int(params['no_of_posts'])+ int(params['no_of_posts'])]
+
+    if page == 1:
+        prev = "#"
+        next = "/?page=" + str(page+1)
+    elif page == last:
+
+        prev = "/?page=" + str(page - 1)
+        next = "#"
+    else:
+        prev = "/?page=" + str(page - 1)
+        next = "/?page=" + str(page + 1)
+
+    return render_template('index.html', params=params, posts=posts, prev=prev, next=next)
 
 
 @app.route("/about")
